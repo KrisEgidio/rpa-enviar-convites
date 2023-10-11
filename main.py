65794d1819d310d858b1f4206ffe114b135126a4
KrisEgidio <56274379+KrisEgidio@users.noreply.github.com>
2023-10-11 10:24:32 -0300

    init

---
+++ main.py
@@ -0,0 +1,147 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+from dotenv import load_dotenv
+import tkinter as tk
+from tkinter import messagebox
+
+def main():
+    load_dotenv()
+    get_login_data()
+    company_id = os.getenv("COMPANY_LINKEDIN_ID")
+    network_url = f"https://www.linkedin.com/company/{company_id}/admin/feed/posts/?invite=true"
+    driver = webdriver.Edge()
+    start_bot(driver, network_url)
+
+
+def get_login_data():
+    """
+    Cria uma janela para input de dados do login
+
+    :return: user, password, company_id
+    """
+    window = tk.Tk()
+    window.title("Login")
+
+    # Crie rótulos para os campos de entrada
+    label_user = tk.Label(window, text="Usuário:")
+    label_password = tk.Label(window, text="Senha:")
+
+    # Crie campos de entrada
+    entry_user = tk.Entry(window)
+    entry_password = tk.Entry(window, show="*")  # A senha é exibida como asteriscos
+
+    button = tk.Button(window, text="Login")
+
+    # Coloque os widgets na janela
+    label_user.pack()
+    entry_user.pack()
+    label_password.pack()
+    entry_password.pack()
+    button.pack()
+
+    # Inicie a janela
+    window.mainloop()
+
+    user = entry_user.get()
+    password = entry_password.get()
+
+    return user, password
+
+def login_to_linkedin(driver):
+    """
+    void
+    Faz login no linkedin. Caso solicite a verificação de segurança, o usuário deve fazer manualmente.
+    """
+    driver.get("https://www.linkedin.com/login")
+    user = driver.find_element(By.ID, "username")
+    password = driver.find_element(By.ID, "password")
+    user.send_keys(os.getenv("LINKEDIN_USERNAME"))
+    password.send_keys(os.getenv("LINKEDIN_PASSWORD"))
+    password.submit()
+
+    ## Verificando se o usuário está logado ##
+    current_url = driver.current_url
+    while 'feed' not in current_url:
+        time.sleep(1)
+        current_url = driver.current_url
+    print("Logged in successfully")
+
+
+def send_requests_to_users(driver, network_url):
+    """
+    void
+    Envia convites para os usuários da rede obtendo a quantidade de convite disponíveis e realizando o cálculo para enviar a quantidade correta.
+    """
+
+    driver.get(network_url)
+    time.sleep(2)
+
+    ## Obtendo a quantidade de convites disponíveis para enviar ##
+    size = driver.find_element(By.XPATH, "//span[@class='t-14 t-black--light']//span[@class='t-bold']").text
+    sends = int(size.split("/")[0])
+
+
+
+    ## Obtendo a quantidade vezes que vai clicar no botão de carregar mais usuários ##
+    if sends >= 20:
+        show_more = round((sends - 20) / 10)
+    else:
+        show_more = 0
+
+    more_results_button = driver.find_element(By.XPATH,
+                                              "//button[@class='artdeco-button artdeco-button--muted artdeco-button--1 artdeco-button--full artdeco-button--secondary ember-view scaffold-finite-scroll__load-button']")
+
+    ## Clicando no botão de carregar mais usuários ##
+    if show_more > 0:
+        for i in range(show_more):
+            more_results_button = driver.find_element(By.XPATH,
+                                                      "//button[@class='artdeco-button artdeco-button--muted artdeco-button--1 artdeco-button--full artdeco-button--secondary ember-view scaffold-finite-scroll__load-button']")
+            more_results_button.click()
+            time.sleep(2)
+
+    ## Selecionando os usuários ##
+    for i in range(sends):
+        checkbox = driver.find_elements(By.XPATH,
+                                        "//input[@class='ember-checkbox ember-view']")
+        driver.execute_script("arguments[0].click();", checkbox[i])
+
+    time.sleep(2)
+
+    ## Enviando os convites ##
+    send_button = driver.find_element(By.XPATH,
+                                      "//button[@class='artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
+    send_button.click()
+    time.sleep(5)
+    print("Requests sent successfully")
+
+
+def take_a_screenshot(driver):
+    """
+    void
+    Tira um print da tela e salva na pasta resource.
+    """
+    loc_time = time.localtime()
+    time_string = time.strftime("%m/%d/%Y", loc_time)
+    driver.save_screenshot(time_string + "_screenshot.png")
+
+
+def start_bot(driver, network_url):
+    """
+    void
+    Inicia o bot.
+    """
+    try:
+        print("Bot started")
+        login_to_linkedin(driver)
+        send_requests_to_users(driver, network_url)
+        driver.quit()
+    except Exception as e:
+        print(f"Something went wrong {e}")
+        # take_a_screenshot(driver)
+        time.sleep(10000000)
+        ##driver.quit()
+
+
+main()
